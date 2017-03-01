b636c2b12b3939c213b977a0557798cc19416b6f
andrit <ritterandrew1@gmail.com>
2017-02-28 21:52:37 -0500

    hook up api with redux ajax

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4a1ff56cafb3cf431a5e88883d07e5eb';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country='us') {
+    const url = `${ROOT_URL}&q=${city},${country}`;
+    const request = axios.get(url);
+
+    return{
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
